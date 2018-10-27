a3cc980dd06ceb552a706b1f343981b6cfe6fcd0
Eetusav <eetu.savolainen@helsinki.fi>
2018-10-27 11:36:23 +0300

    Add eslint to project

--- osa3/models/person.js
+++ osa3/models/person.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const url = process.env.MONGODB_URI
+//const url = process.env.MONGODB_URI
 const Schema = mongoose.Schema
 if ( process.env.NODE_ENV !== 'production' ) {
     require('dotenv').config()
