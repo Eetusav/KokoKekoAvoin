7236cc86eba0381dd89d22d38a8d32f3539286a9
Eetusav <eetu.savolainen@helsinki.fi>
2018-11-02 13:20:17 +0200

    5.5

--- osa5/bloglist-frontend-master/src/App.js
+++ osa5/bloglist-frontend-master/src/App.js
@@ -3,6 +3,7 @@ import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
 import Notification from './components/Notification'
+import loginForm from './components/loginForm'
 
 class App extends React.Component {
   constructor(props) {
@@ -15,7 +16,9 @@ class App extends React.Component {
       user: null,
       title: "",
       author: "",
-      url: ""
+      url: "",
+      hideWhenVisible: '',
+      showWhenVisible: 'none'
 
     }
 
@@ -65,11 +68,11 @@ class App extends React.Component {
   handleBlogTitleChange = (event) => {
     this.setState({ title: event.target.value })
   }
-  handleBlogAuthorChange= (event) => {
-    this.setState({author: event.target.value})
+  handleBlogAuthorChange = (event) => {
+    this.setState({ author: event.target.value })
   }
-  handleBlogUrlChange= (event) => {
-    this.setState({url: event.target.value})
+  handleBlogUrlChange = (event) => {
+    this.setState({ url: event.target.value })
   }
   handlePasswordChange = (event) => {
     this.setState({ password: event.target.value })
@@ -96,29 +99,39 @@ class App extends React.Component {
     const kirjautuneena = () => (
       <div>Kirjautuneena käyttäjällä {this.state.user.username} <button onClick={this.logout}>logout</button></div>
     )
-    const loginForm = () => (
-      <div><h2>Kirjaudu</h2>
+    const loginForm = () => {
+      const hideWhenVisible = { display: this.state.loginVisible ? 'none' : '' }
+      const showWhenVisible = { display: this.state.loginVisible ? '' : 'none' }
+      return (
+        <div>
+          <div style={hideWhenVisible}>
+            <button onClick={e => this.setState({ loginVisible: true })}>log in</button>
+          </div>
+          <div style={showWhenVisible}>
+            <div><h2>Kirjaudu</h2>
+              <form onSubmit={this.login}>
+                <div>
+                  käyttäjätunnus
+        <input
+                    type="text"
+                    value={this.state.username}
+                    onChange={this.handleUsernameChange}
+                  />
+                </div>
+                <div>
+                  salasana
+        <input
+                    type="password"
+                    value={this.state.password}
+                    onChange={this.handlePasswordChange}
+                  />
+                </div>
+                <button type="submit">kirjaudu</button>
+              </form><button onClick={e => this.setState({ loginVisible: false })}>cancel</button></div> </div>
 
-        <form onSubmit={this.login}>
-          <div>
-            käyttäjätunnus
-        <input
-              type="text"
-              value={this.state.username}
-              onChange={this.handleUsernameChange}
-            />
-          </div>
-          <div>
-            salasana
-        <input
-              type="password"
-              value={this.state.password}
-              onChange={this.handlePasswordChange}
-            />
-          </div>
-          <button type="submit">kirjaudu</button>
-        </form></div>
-    )
+        </div>
+      )
+    }
     const blogForm = () => (
       <form onSubmit={this.createNewBlog}>
         <h2>create new</h2>
---
+++ osa5/bloglist-frontend-master/src/components/loginForm.js
@@ -0,0 +1,27 @@
+import React from "react"
+
+const loginForm = () => (
+    <div><h2>Kirjaudu</h2>
+
+      <form onSubmit={this.login}>
+        <div>
+          käyttäjätunnus
+      <input
+            type="text"
+            value={this.state.username}
+            onChange={this.handleUsernameChange}
+          />
+        </div>
+        <div>
+          salasana
+      <input
+            type="password"
+            value={this.state.password}
+            onChange={this.handlePasswordChange}
+          />
+        </div>
+        <button type="submit">kirjaudu</button>
+      </form></div>
+  )
+
+  export default loginForm;
